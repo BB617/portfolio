2173ea6364e9983ef894af32c2ed086331a1d5ad
Kohei Takahashi <libimff0617@gmail.com>
2023-04-12 01:25:21 +0900

    modify max width

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <header className='sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+    <header className='sticky top-0 flex items-start justify-between max-w-5xl mx-auto z-20 xl:items-center'>
       <motion.div
         initial={{
           x: -500,
